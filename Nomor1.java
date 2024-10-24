9cab231d2a243495013fd21ae8c89d680d2baaf1
afdikk <144641714+afdikk@users.noreply.github.com>
2024-10-24 08:42:14 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,5 +1,30 @@
-public class Nomor1{
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Nomor1 {
+    public static void main(String[] args) {
+        // Deklarasi variabel
+        int jamLembur;
+        double honorLembur;
+        int batasJamlembur = 16;
+        double tarifNormal = 10000;
+        double tarifLebih = 15000;
+
+        // Input dari user
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan total jam lembur: ");
+        jamLembur = input.nextInt();
+
+        // Proses perhitungan honor lembur
+        if (jamLembur <= batasJamlembur) {
+            honorLembur = jamLembur * tarifNormal;
+        } else {
+            int lemburLebih = jamLembur - batasJamlembur;
+            honorLembur = (batasJamlembur * tarifNormal) + (lemburLebih * tarifLebih);
+        }
+
+        // Output hasil
+        System.out.println("Total jam lembur: " + jamLembur);
+        System.out.println("Honor lembur: " + honorLembur);
     }
 }
+
